e40630f1705cf83b3887bc89247429a4f8fa8254
Peter Vu <dmtruong1980@gmail.com>
2026-02-21 08:25:17 -0600

    chore: Move sample activity to ExampleApp

--- mobile/app/src/main/java/com/example/vaccinewatch/ExampleApp.kt
+++ mobile/app/src/main/java/com/example/vaccinewatch/ExampleApp.kt
@@ -34,7 +34,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.vaccinewatch.ui.theme.VaccineWatchTheme
 
-class MainActivity : ComponentActivity() {
+class ExampleApp : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
