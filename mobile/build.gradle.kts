0cc8fe45c80207a7d10ca8805360fed9c0f7a811
Peter Vu <dmtruong1980@gmail.com>
2026-02-21 10:41:48 -0600

    chore: Add dependencies electric boogaloo

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -3,3 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+
+implementation(libs.androidx.navigation.compose)
+implementation(libs.androidx.lifecycle.viewmodel.compose)
+implementation(libs.androidx.compose.material.icons.extended)
